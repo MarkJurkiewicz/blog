516ae6033cb09deb6c371c820f8360744cbc11a1
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-07-14 01:43:14 -0700

    more refactoring

--- install.php
+++ install.php
@@ -1,8 +1,9 @@
 <?php
+require_once 'lib/common.php';
+
 // Get the PDO DSN string
-$root = realpath(__DIR__);
-$database = $root . '/data/data.sqlite';
-$dsn = 'sqlite:' . $database;
+$root = getRootPath();
+$database = getDatabasePath();
 
 $error = '';
 
