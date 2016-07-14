102a84e54a771b25d22520620e72255be8505bec
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-07-14 00:53:29 -0700

    displayed rows on html section of file

--- view-post.php
+++ view-post.php
@@ -43,5 +43,15 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     <h1>Blog title</h1>
     <p>This paragraph summarizes what the blog is about.</p>
 
+    <h2>
+        <?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+    </h2>
+    <div>
+        <?php echo $row['created_at'] ?>
+    </div>
+    <p>
+        <?php echo htmlspecialchars($row['body'], ENT_HTML5, 'UTF-8') ?>
+    </p>
+
 </body>
 </html>
