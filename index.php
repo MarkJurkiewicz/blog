7506f896760e9fefa21bb912e0f2ead69ff717bd
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-07-14 00:33:18 -0700

    wrote while loop in html section to display the user posts

--- index.php
+++ index.php
@@ -33,8 +33,19 @@ if ($stmt === false)
 <h1>Blog title</h1>
 <p>This paragraph summarizes what the blog is about.</p>
 
-<p>
-    <a href="#">Read more...</a>
-</p>
+<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+    <h2>
+        <?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+    </h2>
+    <div>
+        <?php echo $row['created_at'] ?>)
+    </div>
+    <p>
+        <?php echo htmlspecialchars($row['body'], ENT_HTML5, 'UTF-8') ?>
+    </p>
+    <p>
+        <a href="#">Read more...</a>
+    </p>
+<?php endwhile ?>
 </body>
 </html>
