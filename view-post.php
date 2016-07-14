8dd14d9ba9b7e164862571c0b224d172e7bdbcb7
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-07-14 01:11:56 -0700

    created if else statement to make sure blog post id is defined

--- view-post.php
+++ view-post.php
@@ -4,6 +4,17 @@ $root = __DIR__;
 $database = $root . '/data/data.sqlite';
 $dsn = 'sqlite:' . $database;
 
+//Get the post ID
+if (isset($_GET['post_id']))
+{
+    $postId = $_GET['post_id'];
+}
+else
+{
+    // this will ensure that a post ID variable is always defined
+    $postId = 0;
+}
+
 // Connection to the database, run a query, handle errors
 $pdo = new PDO($dsn);
 $stmt = $pdo->prepare(
